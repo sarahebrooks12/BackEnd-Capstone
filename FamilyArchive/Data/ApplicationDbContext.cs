3117336f368618254937e83e6b94e2bf91719aab
sarahebrooks12 <sarahebrooks12@gmail.com>
2020-09-24 16:01:56 -0400

    dbcontext updated. seed data is next

--- FamilyArchive/Data/ApplicationDbContext.cs
+++ FamilyArchive/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using FamilyArchive.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -12,5 +13,14 @@ namespace FamilyArchive.Data
             : base(options)
         {
         }
+        public DbSet<Albums> Album { get; set; }
+        public DbSet<Events> Event { get; set; }
+        public DbSet<Family> Family { get; set; }
+        public DbSet<Members> Member { get; set; }
+        public DbSet<Photos> Photo { get; set; }
+        public DbSet<Stories> Story { get; set; }
+        public DbSet<Updates> Updates { get; set; }
+        public DbSet<ApplicationUser> User { get; set; }
+
     }
 }
